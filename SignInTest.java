b3717dff754ed3f391608794ba9be267e1491fcd
batz-chan <pi.alasmawi@gmail.com>
2024-05-11 05:32:46 +0300

    junit test

---
+++ SignInTest.java
@@ -0,0 +1,16 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class SignInTest {
+
+    @org.junit.jupiter.api.Test
+    void authenticate() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void userSignIn() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void main() {
+    }
+}
